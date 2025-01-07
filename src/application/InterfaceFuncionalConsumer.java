572d17d5380f1b474e2c9038e74a50800de143d6
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-07 10:26:01 -0300

    ImplementacaoInterfaceConsumer

--- src/application/InterfaceFuncionalConsumer.java
+++ src/application/InterfaceFuncionalConsumer.java
@@ -1,10 +1,30 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.PriceUpdateConsumer;
+
 public class InterfaceFuncionalConsumer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList();
+		
+		list.add(new Product("Tv", 900.0));
+		list.add(new Product("Mouse", 50.0));
+		list.add(new Product("Tablet", 350.0));
+		list.add(new Product("HD cas",  80.90));
+		
+		list.forEach(new PriceUpdateConsumer());
+		// esse forEach é um defaultMethod que receebe um Consumer como argumento
+		
+		list.forEach(System.out::println);// isso é um referenc
+		// method para o println e faz também impressão na tela
+		
 	}
 
 }
