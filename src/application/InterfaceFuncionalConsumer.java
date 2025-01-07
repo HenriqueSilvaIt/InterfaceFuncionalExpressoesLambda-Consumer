d2998912f199f55504648c99e9b49a078338e6c8
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-07 11:45:22 -0300

    ExpressaoLambdainLine

--- src/application/InterfaceFuncionalConsumer.java
+++ src/application/InterfaceFuncionalConsumer.java
@@ -23,15 +23,22 @@ public class InterfaceFuncionalConsumer {
 		
 		double factor = 1.1;
 		
+		/* Expressão lambda declarada 
 		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
 		// Assim fica bom porque vocÊ consegue passa uma variavel como argumento
-		
-		list.forEach(cons);
+		// é possivel colocar depois do -> abre { e fecha }; e colocar o calculo dentro 
+		// das das chaves, mas n é necesário por isso que n coloquei */
+		//list.forEach(cons);
 		
 		/* list.forEach(Product::nonStaticPriceUpdate); */
 		
 		/*list.forEach(new PriceUpdateConsumer());
 		// esse forEach é um defaultMethod que receebe um Consumer como argumento */
+		
+		// Expressão lambdaInline
+		
+		list.forEach(p -> p.setPrice(p.getPrice() * factor));
+		
 		
 		list.forEach(System.out::println);// isso é um referenc
 		// method para o println e faz também impressão na tela
